3faeed10ae83402b0cea4fc273b368d9a4edaa7c
KatarinaPli <kateryna.shyrokova@powercoders.org>
2023-10-26 16:23:22 +0200

    Made a responsive nav, fixed all ul in nav

--- src/components/Header.js
+++ src/components/Header.js
@@ -24,11 +24,23 @@ export default function Header() {
             </ul>
           </nav>
         </section>
+
+        <div className="hidden md:block">
+          <ul className="flex items-center ml-5">
+            <li className="my-5 md:my-0 md:mr-5">
+              <button className="text-slate-400">Login</button>
+            </li>
+            <li>
+              <button className="btn-defsett rounded-full">Sign Up</button>
+            </li>
+          </ul>
+        </div>
+
         {isOpen && (
           <div className="absolute md:text-left md:py-0 md:flex md:items-center left-5 right-5 top-20 rounded bg-slate-900 text-slate-200 text-center py-10 md:relative md:top-0 md:left-0 md:right-0 md:bg-transparent md:text-slate-700 ">
             {" "}
             <nav className="md:hidden">
-              <ul className="flex items-start justify-start">
+              <ul className="flex flex-col justify-center items-center mb-5">
                 <li>
                   <a href="#">Features</a>
                 </li>
@@ -40,8 +52,8 @@ export default function Header() {
                 </li>
               </ul>
             </nav>
-            <ul className="flex items-start ml-5">
-              <li className="md:mr-5">
+            <ul className="flex flex-col justify-center items-center">
+              <li className="md:mr-5 mb-2">
                 <a href="#">Login</a>
               </li>
               <li>
