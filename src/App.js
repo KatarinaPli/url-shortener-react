c2846ec15cb1cda7ee3f06b7f828b73bacea0d17
KatarinaPli <kateryna.shyrokova@powercoders.org>
2023-11-03 10:02:07 +0100

    Finished Showcase component with simple design

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ export default function App() {
     <div className="App">
       <Header />
       <Showcase />
+      < Footer />
     </div>
   );
 }
--- src/components/Showcase.js
+++ src/components/Showcase.js
@@ -3,15 +3,21 @@ import showcase from "../img/illustration-working.svg";
 
 export default function Showcase() {
   return (
-    <section>
-      <article className="max-width">
-        <h1 className="mb-5 text-4xl">More than just shorter links!</h1>
+    <section className="grid grid-cols-1 gap-5 md:grid-cols-2 md:gap-10 mx-5 lg:mx-40 my-20 lg:my-30">
+      <article className="max-width py-10 lg:py-20 text-center lg:text-left">
+        <h1 className="mb-5 text-4xl text-slate-800">
+          More than just shorter links!
+        </h1>
         <p className="mb-10">
           Build your brand's recognition and get detailed insights on how you
           links are performing
         </p>
         <button className="btn-defsett rounded-full">Get started!</button>
       </article>
+      <aside>
+        {" "}
+        <img src={showcase} alt="Intoduction Shortly" />
+      </aside>
     </section>
   );
 }
