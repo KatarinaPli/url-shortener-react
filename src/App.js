7436aaaca2af32a37d1b1a4ebf1b1300a108e766
KatarinaPli <kateryna.shyrokova@powercoders.org>
2023-11-03 09:43:29 +0100

    Added stucture to the Showcase coponent

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import Header from "./components/Header";
+import Showcase from "./components/Showcase";
 import "./index.css";
-
 
 export default function App() {
   return (
     <div className="App">
-      
       <Header />
+      <Showcase />
     </div>
   );
 }
---
+++ src/components/Showcase.js
@@ -0,0 +1,17 @@
+import React from "react";
+import showcase from "../img/illustration-working.svg";
+
+export default function Showcase() {
+  return (
+    <section>
+      <article className="max-width">
+        <h1 className="mb-5 text-4xl">More than just shorter links!</h1>
+        <p className="mb-10">
+          Build your brand's recognition and get detailed insights on how you
+          links are performing
+        </p>
+        <button className="btn-defsett rounded-full">Get started!</button>
+      </article>
+    </section>
+  );
+}
